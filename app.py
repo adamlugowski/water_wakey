8df0690c0f4fb557e8e766e8a5e75ca9fd940157
Adam Ługowski <adam.lugowski@live.com>
2024-01-16 14:53:22 +0100

    added ValueError. wrote comments to fix all the bugs

--- app.py
+++ app.py
@@ -8,19 +8,28 @@ def main():
     while True:
         time.sleep(60 * 0.1)
         user_response = input('Drink water: ').lower()
-        if user_response == 'ok':
-            with sqlite3.connect('hydration.db') as database:
-                cursor = database.cursor()
-                text = 'H20'
-                today_date = date.today()
-                cursor.execute('INSERT INTO hydrating VALUES (null, ?, ?)', (text, today_date))
-                database.commit()
-                print('Cheers!')
-                for item in stats():
-                    print('You drunk', item, 'times')
-        elif user_response != 'Stop':
-            print("Exiting the program.")
-            break
+        try:
+            if user_response == 'ok':
+                with sqlite3.connect('hydration.db') as database:
+                    cursor = database.cursor()
+                    text = 'H20'
+                    today_date = date.today()
+                    cursor.execute('INSERT INTO hydrating VALUES (null, ?, ?)', (text, today_date))
+                    database.commit()
+                    print('Cheers!')
+                    for item in stats():
+                        # kod pokazuje tuple jednoelementową
+                        print('You drunk', item, 'times')
+            # kod zachowuje się dziwnie przy poniższym kodzie
+            elif user_response == 'stop' or 'Stop':
+                print('Exiting the program. Bye!')
+                # kod nie pokazuje statystyk
+                print(stats())
+                break
+            else:
+                raise ValueError("You should type 'ok'")
+        except ValueError as error:
+            print(error)
 
 
 def db_init(db_cursor):
--- stats.py
+++ stats.py
@@ -4,5 +4,6 @@ import sqlite3
 def stats():
     with sqlite3.connect('hydration.db') as database:
         cursor = database.cursor()
+        # kod zwraca obiekt <sqlite3.Cursor object at 0x000001B00C5CBDC0>
         return cursor.execute('SELECT COUNT(*) FROM hydrating')
 
